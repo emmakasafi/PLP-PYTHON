671e9c8ab36263c31f909f9a562b57c00e2bd064
EmmaculateKatheu <emmakatheu123@gmail.com>
2024-03-15 14:45:00 +0300

    Taking user input

---
+++ userinput.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your current location: ")
+print("Hello, I am " + name + ". I am " + str(age) + " years old and I am currently in " + location + ".")
